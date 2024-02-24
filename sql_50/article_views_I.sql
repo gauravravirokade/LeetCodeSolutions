9fb03eb152b866ff81774481a03913c6f1f5dc36
GauravRTalkiatry <100235301+GauravRTalkiatry@users.noreply.github.com>
2024-02-24 16:33:48 -0500

    1148. Article Views I

---
+++ sql_50/article_views_I.sql
@@ -0,0 +1,9 @@
+-- Write a solution to find all the authors that viewed at least one of their own articles.
+
+SELECT DISTINCT
+    author_id AS id
+FROM views
+WHERE
+    author_id = viewer_id
+ORDER BY
+    author_id;
