b817590b1081f1d49352141b2e38d6af5db3a446
GauravRTalkiatry <100235301+GauravRTalkiatry@users.noreply.github.com>
2024-03-15 09:52:43 -0400

    1693. Daily Leads and Partners

---
+++ SQL/advanced_sql_50/sorting_and_grouping/daily_leads_and_partners.sql
@@ -0,0 +1,13 @@
+-- For each date_id and make_name, find the number of distinct lead_id's and distinct partner_id's.
+-- Return the result table in any order.
+
+SELECT
+    date_id
+  , make_name
+  , COUNT(DISTINCT lead_id)    AS unique_leads
+  , COUNT(DISTINCT partner_id) AS unique_partners
+FROM dailysales
+GROUP BY
+    date_id
+  , make_name;
+
