0918861e23fc4598af6648187e530e571430fd9b
Gaurav Ravishankar Rokade <gauravravirokade@gmail.com>
2024-08-18 15:37:13 -0400

    update comments

--- 30_days_of_pandas/1757_recyclable_and_low_fat_products.py
+++ 30_days_of_pandas/1757_recyclable_and_low_fat_products.py
@@ -1,10 +1,9 @@
-/*
-Write a solution to find the ids of products that are both low fat and recyclable.
 
-Return the result table in any order.
-
-The result format is in the following example.
-*/
+# Write a solution to find the ids of products that are both low fat and recyclable.
+#
+# Return the result table in any order.
+#
+# The result format is in the following example.
 
 import pandas as pd
 
@@ -12,6 +11,3 @@ def find_products(products: pd.DataFrame) -> pd.DataFrame:
     low_fat_and_recyclable_filter = ( (products["low_fats"] == 'Y') & (products["recyclable"] == 'Y' ))
     low_fat_and_recyclable_products = products["product_id"].where(low_fat_and_recyclable_filter)
     return low_fat_and_recyclable_products.dropna().to_frame()
-
-
-
